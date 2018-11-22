948fefcfc4379f5af1b05731b70d2525d7f45e0e
Yuriy Luchaninov <yuriy.luchaninov@gmail.com>
2018-11-22 20:15:18 +0200

    small env fix $)

--- app/abac/env.js
+++ app/abac/env.js
@@ -1,6 +1,8 @@
 const env = {
-    /** be careful with the time zone **/
-    time: new Date()
+  /** be careful with the time zone **/
+  get time() {
+    return new Date();
+  }
 };
 
 module.exports = env;
